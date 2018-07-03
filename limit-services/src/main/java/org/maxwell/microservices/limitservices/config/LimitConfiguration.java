49fbf09efedd2492c133f87ba7ae12601dc1955c
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-07-03 12:56:35 -0700

    first commit

---
+++ limit-services/src/main/java/org/maxwell/microservices/limitservices/config/LimitConfiguration.java
@@ -0,0 +1,25 @@
+package org.maxwell.microservices.limitservices.config;
+
+public class LimitConfiguration {
+
+	private int maximum;
+	private int minimum;
+
+	public LimitConfiguration() {
+
+	}
+
+	public LimitConfiguration(int maximum, int minimum) {
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+}
