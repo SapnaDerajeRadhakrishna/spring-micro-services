54bbb2c47bef934f3df734ef008b39496d42eb96
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-07-05 10:46:11 -0700

    Used spring cloud bus for group refresh of values

--- limit-services/src/main/java/org/maxwell/microservices/limitservices/controller/LimitServiceController.java
+++ limit-services/src/main/java/org/maxwell/microservices/limitservices/controller/LimitServiceController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitServiceController {
 
@@ -16,4 +18,15 @@ public class LimitServiceController {
 	public LimitConfiguration retrieveLimits() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	}
+
 }
